e04de45d83ac5637832c92b3663a94f5bbb2fefb
Abhishek Sharma <abhisheksharma86490@gmail.com>
2021-01-17 14:24:56 +0530

    modifying oops

--- oops1.cpp
+++ oops1.cpp
@@ -1,12 +1,17 @@
 #include<iostream>
+#include <cstring>
 using namespace std;
 class Student{
     private:
     int RollNo;
     int StudentID;
+    char *name;
     
     public:
-    Student(int RollNo, int StudentID){
+    Student(int RollNo, int StudentID,char *name){
+        //Deep copy
+        this -> name = new char[strlen(name) + 1];
+        strcpy(this->name,name);
         this -> RollNo = RollNo;
         this -> StudentID = StudentID;
     }
@@ -22,21 +27,31 @@ class Student{
     int getStudentID(){
         return StudentID;
     }
+    string getName(){
+        return name;
+    }
     void Display(){
         cout<<"Roll No.: "<<getRollNo()<<endl;
         cout<<"Student ID : "<<getStudentID()<<endl;
+        cout<<"Student name: "<<getName()<<endl;
     }
 };
 
 
 int main(){
-    Student s1(12,11);
+    char name[]= "abhi";
+    Student s1(12,11,name);
     s1.Display();
-    Student * s2 = new Student(121, 113);
-    s2 -> Display();
-    s2 -> setRollNo(112);
-    s2 -> setStudentID(123);
-    s2 -> Display();
-    delete s2;
+    name[3]= 'c';
+    
+    Student s2(122 , 111 , name);
+    s2.Display();
+
+    // Student * s2 = new Student(121, 113,a);
+    // s2 -> Display();
+    // s2 -> setRollNo(112);
+    // s2 -> setStudentID(123);
+    // s2 -> Display();
+    // delete s2;
     return 0;
 }
