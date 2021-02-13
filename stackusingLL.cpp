30647626b65803ecb53698e6d8101bb9cf8850d1
Abhishek Sharma <abhisheksharma86490@gmail.com>
2021-02-13 12:11:12 +0530

    adding stack using LL

---
+++ stackusingLL.cpp
@@ -0,0 +1,76 @@
+#include<iostream>
+using namespace std;
+class Node{
+    public:
+    int data;
+    Node* next;
+    Node(int data){
+        this->data = data;
+    }
+};
+class Stack{
+    Node* head;
+    int count=0;
+
+    public:
+    void push(int element){
+        if(head==NULL){
+            Node* n = new Node(element);
+            head=n;
+            count++;
+        }
+        else{
+            Node* n = new Node(element);
+            
+            n->next=head;
+            head=n;
+            count++;
+        }
+    }
+    int pop(){
+        int a;
+        if(head==NULL){
+            cout<<"stack is empty"<<endl;
+            return 0;
+        }
+        else{
+            a = head->data;
+            head = head->next;
+            count--;
+            return a;
+           
+        }
+    }
+    int top(){
+        if(head==NULL){
+            cout<<"stack is empty"<<endl;
+            return 0;
+        }
+        else{
+            return head->data;
+        }
+    }
+    int size(){
+        return count;
+    }
+    bool IsEmpty(){
+        return !count;
+    }
+};
+
+int main(){
+    Stack s;
+    s.push(10);
+    s.push(20);
+    s.push(30);
+    s.push(40);
+    s.push(50);
+    cout<<s.top()<<endl;
+    cout<<s.pop()<<endl;
+    cout<<s.pop()<<endl;
+    cout<<s.pop()<<endl;
+    cout<<s.pop()<<endl;
+    cout<<s.IsEmpty()<<endl;
+    cout<<s.size()<<endl;
+
+}
