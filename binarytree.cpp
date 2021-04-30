77c6c520a80f2ff5f9bc33dda12a27031a94b73c
Abhishek Sharma <abhisheksharma86490@gmail.com>
2021-04-30 19:56:55 +0530

    construct tree using inorder and preorder data

--- binarytree.cpp
+++ binarytree.cpp
@@ -71,8 +71,12 @@ void printlevelwise(BinaryTreeNode<int>* root){
         return;
     }
     cout<<root->data<<": ";
-    cout<<"L"<<root->left->data<<", ";
-    cout<<"R"<<root->right->data<<endl;
+    if(root->left != NULL){
+        cout<<"L"<<root->left->data<<", ";
+    }
+    if(root->right != NULL){
+        cout<<"R"<<root->right->data<<endl;
+    }
     queue<BinaryTreeNode<int>*> pending;
     pending.push(root);
 
@@ -81,14 +85,24 @@ void printlevelwise(BinaryTreeNode<int>* root){
         pending.pop();
         if(front->left){
             cout<<front->left->data<<":";
-            cout<<"L"<<front->left->left->data<<", ";
-            cout<<"R"<<front->left->right->data<<endl;
+            if(front->left->left != NULL){
+                cout<<"L"<<front->left->left->data<<", ";
+
+            }
+            if(front->left->right != NULL){
+                cout<<"R"<<front->left->right->data<<endl;
+            }
             pending.push(front->left);
         }
         if(front->right){
             cout<<front->right->data<<":";
-            cout<<"L"<<front->right->left->data<<", ";
-            cout<<"R"<<front->right->right->data<<endl;
+            if(front->right->left != NULL){
+                cout<<"L"<<front->right->left->data<<", ";
+            }
+            if(front->right->right != NULL){
+                cout<<"R"<<front->right->right->data<<endl;
+
+            }
             pending.push(front->right);
         }
     }
@@ -110,7 +124,64 @@ void printData(BinaryTreeNode<int>* root){
     printData(root->left);
     printData(root->right);
 }
+
+void inorder(BinaryTreeNode<int>* root){
+    if(root == NULL){
+        return;
+    }
+    inorder(root->left);
+    cout<<root->data<<" ";
+    inorder(root->right);
+}
+BinaryTreeNode<int>* helpbuildTree(int* in, int* pre, int InS, int InE, int preS, int preE ){
+    if(InS > InE){
+        return NULL;
+    }
+    int rootData = pre[preS];
+    int rootindex = -1;
+    for(int i = InS ;i<=InE; i++){
+        if(in[i] == rootData){
+            rootindex=i;
+            break;
+        }
+    }
+    int lInS = InS;
+    int lInE = rootindex-1;
+    int lpreS = preS + 1;
+    int lpreE = lInE-lInS+lpreS;
+    int rInS = rootindex+1;
+    int rInE = InE;
+    int rpreS = lpreE+1;
+    int rpreE = preE;
+    BinaryTreeNode<int>* root = new BinaryTreeNode<int>(rootData);
+    root->left = helpbuildTree(in, pre, lInS, lInE, lpreS, lpreE);
+    root->right = helpbuildTree(in, pre, rInS, rInE, rpreS, rpreE);
+    return root;
+
+}
+BinaryTreeNode<int>* buildTree(int* in, int* pre, int size){
+    return helpbuildTree(in, pre, 0, size-1, 0, size-1);
+} 
+
+int numNode(BinaryTreeNode<int>* root){
+    if(root == NULL){
+        return 0;
+    }
+    return 1+numNode(root->left)+numNode(root->right);
+}
 int main(){
+    int in[] = {4,2,5,1,8,6,9,3,7};
+    int pre[] = {1,2,4,5,3,6,8,9,7};
+
+    BinaryTreeNode<int>* root = buildTree(in, pre, 9);
+    printlevelwise(root);
+    delete root;
+
+    /*
     BinaryTreeNode<int>* root = TakeInputLevelwise();
     printlevelwise(root);
+    cout<<"The Number of Nodes: "<<numNode(root)<<endl;
+    inorder(root);
+    delete root;
+    */
 }
